13087ee26180ca3841938174f5ca22527c4d46c7
Andrii Bykov <Andrii.Bykov@infopulse.com>
2021-11-30 18:04:54 +0200

    cdc

--- ProjectForGit/Class1.cs
+++ ProjectForGit/Class1.cs
@@ -7,12 +7,11 @@ namespace ProjectForGit
         public int a = 56;
         public int b = 3;
         public int c = -4;
-        public int d = -4;
 
         public void Test(int a)
         {
             Console.WriteLine("Test");
-
+            Console.WriteLine("Test");
         }
     }
 }
