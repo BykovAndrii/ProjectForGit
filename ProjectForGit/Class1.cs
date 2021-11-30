37d6baf78568102e8d58115187f307d01303fd2c
Andrii Bykov <Andrii.Bykov@infopulse.com>
2021-11-30 17:10:37 +0200

    we4rtf

--- ProjectForGit/Class1.cs
+++ ProjectForGit/Class1.cs
@@ -8,9 +8,9 @@ namespace ProjectForGit
         public int b = 3;
         public int c = -2;
 
-        public void Test1(int a)
+        public void Test(int a)
         {
-            Console.WriteLine("Test1");
+            Console.WriteLine("Test2");
         }
 
         public void Test1()
